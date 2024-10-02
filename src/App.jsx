32c586d058d2cec55aad9e70f4f7469237b208b0
sanchxt <tpbeast007@gmail.com>
2024-10-03 01:55:49 +0530

    add skybox

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,14 @@
+import { Sky } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+
 function App() {
-  return <div>hi</div>;
+  return (
+    <>
+      <Canvas>
+        <Sky sunPosition={[100, 100, 20]} />
+      </Canvas>
+    </>
+  );
 }
 
 export default App;
