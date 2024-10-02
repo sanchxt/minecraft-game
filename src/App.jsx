10f5fa2f9db30f98041796cbaa09ea06806f74a6
sanchxt <tpbeast007@gmail.com>
2024-10-03 02:36:06 +0530

    add lighting, import cubes

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import { Sky } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
+import { Cubes } from "./components/Cubes";
 
 function App() {
   return (
     <>
       <Canvas>
         <Sky sunPosition={[100, 100, 20]} />
+        <ambientLight intensity={0.5} />
+        <Physics>
+          <Cubes />
+        </Physics>
       </Canvas>
     </>
   );
