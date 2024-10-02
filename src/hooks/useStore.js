941923ddaed4665d0e30df816b77b12f18eb8a64
sanchxt <tpbeast007@gmail.com>
2024-10-03 02:37:10 +0530

    create hooks

---
+++ src/hooks/useStore.js
@@ -0,0 +1,46 @@
+import { create } from "zustand";
+import { nanoid } from "nanoid";
+
+const getLocalStorage = (key) => JSON.parse(window.localStorage.getItem(key));
+const setLocalStorage = (key, value) =>
+  window.localStorage.setItem(key, JSON.stringify(value));
+
+export const useStore = create((set, get) => ({
+  texture: "dirt",
+  cubes: getLocalStorage("cubes") || [],
+  addCube: (x, y, z) => {
+    set((prev) => ({
+      cubes: [
+        ...prev.cubes,
+        {
+          key: nanoid(),
+          pos: [x, y, z],
+          texture: prev.texture,
+        },
+      ],
+    }));
+  },
+  removeCube: (x, y, z) => {
+    set((prev) => ({
+      cubes: prev.cubes.filter((cube) => {
+        const [X, Y, Z] = cube.pos;
+        return X !== x || Y !== y || Z !== z;
+      }),
+    }));
+  },
+  setTexture: (texture) => {
+    set(() => ({
+      texture,
+    }));
+  },
+  saveWorld: () => {
+    const cubes = get().cubes;
+    setLocalStorage("cubes", cubes);
+  },
+  resetWorld: () => {
+    set(() => ({
+      cubes: [],
+    }));
+    setLocalStorage("cubes", []);
+  },
+}));
